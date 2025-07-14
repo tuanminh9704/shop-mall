452acc447230a8a6882423184c8be981126a0f3a
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2025-07-14 20:15:23 +0700

    feature updated app module with correct module

--- shop-mall-server/src/app.module.ts
+++ shop-mall-server/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
-import { CategoriesModule } from './categories/categories.module';
-import { OrdersModule } from './modules/orders/orders.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 @Module({
-  imports: [ProductsModule, CategoriesModule, OrdersModule],
+  imports: [ProductsModule, CategoriesModule],
 })
 export class AppModule {}
